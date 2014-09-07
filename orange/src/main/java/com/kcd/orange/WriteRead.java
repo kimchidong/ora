d045903759c087ec6b92ee39c7a2e57493e2ad7a
kimchidong <chidong2@naver.com>
2014-09-08 06:27:10 +0900

    2

---
+++ orange/src/main/java/com/kcd/orange/WriteRead.java
@@ -0,0 +1,44 @@
+package com.kcd.orange;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class WriteRead
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String parentPath = "D:/file/";
+			String s = "Hello, World! 안녕?";
+
+			byte[] by = s.getBytes();
+
+			ByteBuffer buf = ByteBuffer.allocate(by.length);
+			buf.put(by);
+			buf.clear();
+
+			FileOutputStream f_out = new FileOutputStream(parentPath + "1.txt");
+			FileChannel out = f_out.getChannel();
+			out.write(buf);
+			out.close();
+
+			FileInputStream f_in = new FileInputStream(parentPath + "1.txt");
+			FileChannel in = f_in.getChannel();
+
+			ByteBuffer buf2 = ByteBuffer.allocate((int)in.size());
+			in.read(buf2);
+
+			byte[] by2 = buf2.array();
+			String s2 = new String(by2);
+
+			System.out.println(s2);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
